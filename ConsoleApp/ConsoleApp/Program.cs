8f14797ac2dd6ad0fcada685ec44a6fc9c137fec
isolog91 <ilya.sia@yandex.ru>
2021-11-01 21:22:49 +0300

    updates

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World! from isolog");
+        }
+    }
+}
